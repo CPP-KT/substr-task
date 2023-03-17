3a6aa218115b97c0d1597a5b816fac2b7611868e
Artem Koton <kotonartem@protonmail.com>
2023-03-17 23:52:18 +0300

    Only treat warnings as errors in CI build

--- build.sh
+++ build.sh
@@ -2,18 +2,18 @@
 
 mkdir _build_debug_san
 cd _build_debug_san
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined" ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined" -DTREAT_WARNINGS_AS_ERRORS=1 ..
 cmake --build .
 cd ..
 
 mkdir _build_rwdi_san
 cd _build_rwdi_san
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined" ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined" -DTREAT_WARNINGS_AS_ERRORS=1 ..
 cmake --build .
 cd ..
 
 mkdir _build_release
 cd _build_release
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DTREAT_WARNINGS_AS_ERRORS=1 ..
 cmake --build .
 cd ..
