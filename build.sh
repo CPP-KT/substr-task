a563d47b289897ed12f37d0403b5f51075f1a3d4
Artem Koton <kotonartem@protonmail.com>
2024-03-08 17:46:19 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-mkdir _build_debug_san
-cd _build_debug_san
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined" -DTREAT_WARNINGS_AS_ERRORS=1 ../src
-cmake --build .
-cd ..
+BUILD_TYPE=${1:-SanitizedDebug}
 
-mkdir _build_rwdi_san
-cd _build_rwdi_san
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_CXX_FLAGS="-fsanitize=address,undefined" -DTREAT_WARNINGS_AS_ERRORS=1 ../src
-cmake --build .
-cd ..
+# Configure CMake
+cmake -S . -B cmake-build-"$BUILD_TYPE" --preset "$BUILD_TYPE"
 
-mkdir _build_release
-cd _build_release
-cmake -DCMAKE_BUILD_TYPE=Release -DTREAT_WARNINGS_AS_ERRORS=1 ../src
-cmake --build .
-cd ..
+# Build
+cmake --build cmake-build-"$BUILD_TYPE"
